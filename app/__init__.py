f8ae7b8f096d1bbc6a50a151dc167170f98f5863
Jeffrey Mew <t-jemew@microsoft.com>
2018-12-04 19:16:48 -0800

    Enable production mode

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ STATIC_FOLDER = os.path.join(APP_DIR, '../static/build/static') # Where your web
 TEMPLATE_FOLDER = os.path.join(APP_DIR, '../static/build') # Where your index.html file is located
 
 app = Flask(__name__, static_folder=STATIC_FOLDER, template_folder=TEMPLATE_FOLDER)
-app.config.from_object('app.config.TestingConfig')
+app.config.from_object('app.config.ProductionConfig')
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
